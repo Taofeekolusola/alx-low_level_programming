95e5a5d218d855c83bcc8ca9448211bc56ea409f
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-18 14:32:59 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that assign a value to the variable n each time it is executed
+ * return 0
+ */
+int main(void)
+{
+	int n  = 20;
+	if(n > 0){printf ("the number is positive\n");}
+	else if(n == 0) {printf("the number is zero\n");}
+	else (n < 0); {printf("the number is negative\n");}
+	return 0;
+}
