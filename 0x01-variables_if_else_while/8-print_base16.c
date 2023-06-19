db3eddbbefdfb5ca9aba6e194a3ac1029da39e93
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-19 15:15:12 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@
 int main(void)
 {
 	int num;
+	char alph;
 
-	for (num = 0; num <= 16; num++)
+	for (num = '0'; num <= '9'; num++)
 	putchar(num);
+
+	for (alph = 'a'; alph <= 'f'; alph++)
+	putchar(alph);
 	putchar('\n');
 
 	return (0);
