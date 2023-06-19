a550c9b19f4fe92d774b5335c93146a61965a303
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-19 15:04:50 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,5 +5,14 @@
  * main - all the numbers of base 16
  * Return: Always 0 (Success)
  */
-int main(void)
 
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 16; num++)
+	putchar(num);
+	putchar('\n');
+
+	return (0);
+}
