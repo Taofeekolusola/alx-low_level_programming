1abc233249d487ec165b08aa315e92834e3eb682
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-21 06:26:54 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (c <= '9')
+	{
+	for (c = 'd'; c <= '3'; c++)
+	{
+	_putchar(c);
+
+	_putchar('\n');
+	}
+	i++;
+	}
+}
