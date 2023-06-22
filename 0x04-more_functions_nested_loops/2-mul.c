aed7ac772cf43ad879ab08987b93453e2fade335
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-22 11:50:19 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 /**
  * mul - multiplies two integers
  * @x: number to multiply
- * @y: second number to multiply 
- * Return: the result of x mult by y
+ * @y: second number to multiply
+ * Return: the result of x multiplied by y
  */
 
 int mul(int x, int y)
