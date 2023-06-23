e38d10b94cb647934f6f07a5f05d608b7e77a633
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-23 13:33:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-	if ((x == % 3) && (x == % 5))
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
 	printf("FizzBuzz");
 	}
-	else if (x == % 3)
+	else if (x % 3 == 0)
 	{
 	printf("Fizz");
 	}
-	else if (x == % 5)
+	else if (x % 5 == 0)
 	{
 	printf("Buzz");
 	}
