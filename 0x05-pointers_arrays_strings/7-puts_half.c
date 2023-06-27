baf763f5088bc75bd7b5788818e5982b6f8ae063
Taofeekolusola <olusolataofeek@gmail.com>
2023-06-27 04:18:47 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,24 +8,24 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int k;
-
+	
 	while (str[i] != '\0')
 	{
 	i++;
 	}
-       if (i % 2 == 1)
+ 	if (i % 2 == 1)
 	{
 	k = (i - 1) / 2;
 	k += i;
 	}
-       else
+ 	else
 	{
 	k = i / 2;
 	}
-
-       for (; k < i; k++)
-       {
-	_putchar(str[k]);
-       }
-       _putchar('\n');
+       
+	for (; k < i; k++)
+ 	{
+	_putchar(str[k])
+       	}
+ 	_putchar('\n');
 }
