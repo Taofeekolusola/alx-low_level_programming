59993119e99e335b3d62779f0bf887c5b3db6ece
Taofeekolusola <olusolataofeek@gmail.com>
2023-07-04 00:51:35 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * _strchr - Entry point
  * @s: Input
  * @c: Input
  * Return: Always 0 (Success)
  */
+
 char *_strchr(char *s, char c)
 {
 	int a;
