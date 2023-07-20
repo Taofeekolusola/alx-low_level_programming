8a49f23f3bf8d7c9d58ec362c61d0d9a1b607b83
Taofeekolusola <olusolataofeek@gmail.com>
2023-07-20 09:55:04 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,16 +2,15 @@
 /**
  * print_name - prints a name
  * @name: the name to be printed
+ * @f: pointer to function
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	_putchar("my name is taofeek");
-	_putchar("\n");
-}
-int main()
-{
-	char a;
-	void (*ptr)(char*, void (*)(char*)) = print_name
-	ptr("Taofeek, (Olusola)");
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+
+	f(name);
 }
