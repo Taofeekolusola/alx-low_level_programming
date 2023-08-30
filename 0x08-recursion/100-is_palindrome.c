63ebfa4abb3f24105004bb10799766ebd7700c17
TaofeekOlusola <olusolataofeek@gmail.com>
2023-08-30 17:40:45 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,7 +5,6 @@ int _lent_recursion(char *c);
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
- *
  * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
