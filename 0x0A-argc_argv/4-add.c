d18c25133dc1daa3eae03cec543122bbebe6a39d
TaofeekOlusola <olusolataofeek@gmail.com>
2023-09-01 15:08:45 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,66 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * main -  adds positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: Always zero
+ * is_digit - check - string there are digit
+ * @dig: array dig
+ *
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int is_digit(char *dig)
 {
-	int i,  sum = 0;
+	unsigned int i;
 
-	if (argc == 0)
-	{
-		printf("Zero\n");
-	}
-	if (argc != int)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (argc > 1)
-	{
+	i = 0;
+	while (i < strlen(dig))
 
-		for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(dig[i]))
 		{
-			printf("argv[%d] = %s\n", i, argv[i]);
-			sum += atoi(argv[i]);
+			return (0);
 		}
-		printf("sum = %d\n", sum);
+
+
+		i++;
 	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+	int str_to_int;
+	int sum = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (is_digit(argv[i]))
+		{
+			str_to_int = atoi(argv[i]);
+			sum += str_to_int;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		i++;
+	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }
